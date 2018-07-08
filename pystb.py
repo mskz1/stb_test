2221af78770fd7ea468b1577c0e9dbb9c0915705
mskz1 <mskz.tsuji@me.com>
2018-07-08 20:32:03 +0900

    修正

--- pystb.py
+++ pystb.py
@@ -2,6 +2,9 @@ import xml.etree.ElementTree as ET
 
 STB_NODE, STB_X_AXIS, STB_Y_AXIS, STB_STORY = 'StbNode', 'StbX_Axis', 'StbY_Axis', 'StbStory'
 STB_COLUMN, STB_GIRDER, STB_BEAM, STB_BRACE = 'StbColumn', 'StbGirder', 'StbBeam', 'StbBrace'
+STB_SEC_COLUMN_S, STB_SEC_STEEL_COLUMN = 'StbSecColumn_S', 'StbSecSteelColumn'
+STB_SEC_BEAM_RC, STB_SEC_FIG = 'StbSecBeam_RC', 'StbSecFigure'
+STB_SEC_BEAM_S, STB_SEC_STEEL_BEAM = 'StbSecBeam_S', 'StbSecSteelBeam'
 
 
 class Stb:
@@ -12,11 +15,11 @@ class Stb:
         with open(file, 'r', encoding='Shift_JIS') as f:
             self.stb = ET.fromstring(f.read())
 
-    def get_max_node_id(self):
+    def get_max_id(self, tag):
         ids = []
         if self.stb:
-            for n in self.stb.iter('StbNode'):
-                ids.append(int(n.attrib['id']))
+            for d in self.stb.iter(tag):
+                ids.append(int(d.attrib['id']))
         return max(ids)
 
     def get_node_numbers(self):
@@ -47,17 +50,10 @@ class Stb:
                     return float(n.attrib['z'])
         return None
 
-    def get_x_axis_names(self):
+    def get_name_list(self, tag):
         res = []
         if self.stb:
-            for d in self.stb.iter('StbX_Axis'):
-                res.append(d.attrib['name'])
-        return res
-
-    def get_y_axis_names(self):
-        res = []
-        if self.stb:
-            for d in self.stb.iter('StbY_Axis'):
+            for d in self.stb.iter(tag):
                 res.append(d.attrib['name'])
         return res
 
@@ -73,25 +69,11 @@ class Stb:
                 if d.attrib['name'] == name:
                     return float(d.attrib['distance'])
 
-    def get_story_names(self):
-        res = []
-        if self.stb:
-            for d in self.stb.iter('StbStory'):
-                res.append(d.attrib['name'])
-        return res
-
     def get_story_height(self, name=''):
         if self.stb:
             for d in self.stb.iter('StbStory'):
                 if d.attrib['name'] == name:
                     return float(d.attrib['height'])
-
-    def get_max_column_id(self):
-        ids = []
-        if self.stb:
-            for n in self.stb.iter('StbColumn'):
-                ids.append(int(n.attrib['id']))
-        return max(ids)
 
     def get_column_numbers(self):
         ids = []
@@ -101,19 +83,17 @@ class Stb:
         return len(ids)
 
     def get_element_attribute(self, tag, **kwargs):
+        # **kwargsで複数のパラメーター条件を指定し、該当するエレメントの（アトリビュート）のリストを返す
+        # 複数の条件の全てに該当するものが集められる。
+        res = []
         for d in self.stb.iter(tag):
+            flg = []
             for k, v in kwargs.items():
                 if d.attrib[k] == v:
-                    return d.attrib
-
-    def get_elements(self,tag, **kwargs):
-        # **kwargsで複数のパラメーター条件を指定し、該当するエレメントのリストを返す？
-        # 複数の条件のいずれかに該当するものが集められる。
-        res = []
-        for k, v in kwargs.items():
-            for d in self.stb.iter(tag):
-                if d.attrib[k] == v:
-                    # res.append(d)
-                    res.append(d.attrib)
+                    flg.append(True)
+                else:
+                    flg.append(False)
+            if False not in flg:
+                # res.append(d) #あるいはエレメントを返す？
+                res.append(d.attrib)
         return res
-
--- test_pystb.py
+++ test_pystb.py
@@ -1,4 +1,6 @@
-from pystb import Stb, STB_NODE, STB_X_AXIS
+from pystb import Stb, STB_NODE, STB_X_AXIS, STB_Y_AXIS, STB_STORY, STB_COLUMN, STB_GIRDER, STB_BEAM, STB_BRACE
+from pystb import STB_SEC_COLUMN_S, STB_SEC_STEEL_COLUMN
+from pystb import STB_SEC_BEAM_S, STB_SEC_STEEL_BEAM
 import pytest
 from pytest import approx
 
@@ -12,32 +14,33 @@ def stb():
 
 
 def test_node_max(stb):
-    assert stb.get_max_node_id() == 382
+    assert stb.get_max_id(STB_NODE) == 382
     assert stb.get_node_numbers() == 148
 
 
 def test_node_coordinates(stb):
     assert stb.get_node_x(id=5) == approx(7200)
+    assert stb.get_element_attribute(STB_NODE, id='5')[0]['x'] == '7200'
     assert stb.get_node_y(id=5) == approx(0)
+    assert stb.get_element_attribute(STB_NODE, id='5')[0]['y'] == '0'
     assert stb.get_node_z(id=5) == approx(150)
+    assert stb.get_element_attribute(STB_NODE, id='5')[0]['z'] == '150'
     assert stb.get_node_x(id=104) == approx(21600)
     assert stb.get_node_y(id=104) == approx(12600)
     assert stb.get_node_z(id=350) == approx(4350)
     assert stb.get_node_x(id=93) is None
 
 
-def test_axis_names(stb):
-    assert stb.get_x_axis_names() == ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13']
-    assert stb.get_y_axis_names() == ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
+def test_name_list(stb):
+    assert stb.get_name_list(STB_X_AXIS) == ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10', '11', '12', '13']
+    assert stb.get_name_list(STB_Y_AXIS) == ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I']
+    assert stb.get_name_list(STB_STORY) == ['1F', 'MF', 'RF']
 
 
 def test_axis_coordinates(stb):
     assert stb.get_x_axis_distance('3') == approx(3600)
+    assert stb.get_element_attribute(STB_X_AXIS, name='3')[0]['distance'] == '3600'
     assert stb.get_y_axis_distance('G') == approx(10800)
-
-
-def test_story_names(stb):
-    assert stb.get_story_names() == ['1F', 'MF', 'RF']
 
 
 def test_story_height(stb):
@@ -45,16 +48,31 @@ def test_story_height(stb):
 
 
 def test_column_max(stb):
-    assert stb.get_max_column_id() == 82
+    assert stb.get_max_id(STB_COLUMN) == 82
     assert stb.get_column_numbers() == 82
 
 
-def test_get_element_data(stb):
-    assert stb.get_element_attribute(STB_NODE, id='6') == {'id': '6', 'x': '9000', 'y': '0', 'z': '150',
-                                                           'kind': 'ON_GRID'}
-
-
 def test_get_elements(stb):
-    li = stb.get_elements(STB_NODE, y='0', z='150')
+    # li = stb.get_element_attribute(STB_NODE, y='0', z='150')
+    # for i in li:
+    #     print(i)
+    # li = stb.get_element_attribute(STB_COLUMN, name='C3')
+    # for i in li:
+    #     print(i)
+    print()
+    print('col_shape------')
+    li = stb.get_element_attribute(STB_SEC_STEEL_COLUMN)
     for i in li:
         print(i)
+    print('beam_shape------')
+    li = stb.get_element_attribute(STB_SEC_STEEL_BEAM)
+    for i in li:
+        print(i)
+
+    assert stb.get_element_attribute(STB_NODE, id='6')[0] == {'id': '6', 'x': '9000', 'y': '0', 'z': '150',
+                                                              'kind': 'ON_GRID'}
+    assert len(stb.get_element_attribute(STB_NODE)) == 148
+    assert stb.get_element_attribute(STB_COLUMN, id='1')[0] == {'id': '1', 'name': 'C3', 'idNode_bottom': '1',
+                                                                'idNode_top': '118', 'rotate': '0', 'id_section': '1',
+                                                                'kind_structure': 'S', 'offset_X': '0', 'offset_Y': '0',
+                                                                'offset_bottom_Z': '-1050', 'condition_bottom': 'FIX'}
