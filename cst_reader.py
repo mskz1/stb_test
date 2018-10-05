fa9e1ccaf8c7a2eccf97762c90393b5e9eb2446f
mskz1 <mskz.tsuji@me.com>
2018-10-05 18:11:14 +0900

    編集

--- cst_reader.py
+++ cst_reader.py
@@ -1,22 +1,89 @@
 import os
 import sys
+import glob
 
 BUKKEN_CSV_NAME = "Structure\Analysis\Cooperation_Bukken.csv"
 BUZAI_CSV_NAME = 'Structure\Analysis\Cooperation_Buzai.csv'
+BUZAI_KIGOU_FOLDER_PATH = "Master"
+
+
+class CasstData:
+    def __init__(self):
+        self.sec_data = {}
+        self.buzai = {}
+
+    def _load_section_data(self, f, res):
+        lines = f.readlines()
+        section_index = []
+        for i, line in enumerate(lines):
+            if line[:7] == "SECTION":
+                section_index.append(i)
+        for i, idx in enumerate(section_index):
+            section_name = lines[idx].split(sep=',')[1]
+            if i < len(section_index) - 1:
+                data = lines[idx + 1:section_index[i + 1]]
+            else:
+                data = lines[idx + 1:]
+            res[section_name] = data
+
+    def _load_buzai_data(self, folder_name):
+        file_names = glob.glob(os.path.join(folder_name, BUZAI_KIGOU_FOLDER_PATH, 'list_buzaikigou_*.csv'))
+        for file_name in file_names:
+            with open(file_name) as f:
+                kigou = os.path.basename(file_name)[:-4].split(sep='_')[-1]
+                lines = f.readlines()[1:]  # １行目は捨てる
+                self.buzai[kigou] = lines
+
+    def _buzaikigou_file_names(self, folder_name):
+        file_names = glob.glob(os.path.join(folder_name, BUZAI_KIGOU_FOLDER_PATH, 'list_buzaikigou_*.csv'))
+        return file_names
+
+    def load(self, folder_name):
+        with open(os.path.join(folder_name, BUKKEN_CSV_NAME)) as f:
+            self._load_section_data(f, self.sec_data)
+        with open(os.path.join(folder_name, BUZAI_CSV_NAME)) as f:
+            self._load_section_data(f, self.sec_data)
+        self._load_buzai_data(folder_name)
+
+    def get_section_data(self, section, idx=None):
+        if idx:
+            data = self.sec_data[section]
+            if len(data) == 1:
+                return data[0].split(sep=',')[idx - 1]
+            else:
+                res = []
+                for d in data:
+                    res.append(d.split(sep=',')[idx - 1])
+                return res
+        else:
+            return self.sec_data[section]
+
+    def get_buzai_data(self, kigou, idx=None):
+        if idx:
+            data = self.buzai[kigou]
+            if len(data) == 1:
+                return data[0].split(sep=',')[idx - 1]
+            else:
+                res = []
+                for d in data:
+                    res.append(d.split(sep=',')[idx - 1])
+                return res
+        else:
+            return self.buzai[kigou]
 
 
 def read_cst(folder_name):
+    # 不要
     res = {}
     with open(os.path.join(folder_name, BUKKEN_CSV_NAME)) as f:
         load_section_data(f, res)
     with open(os.path.join(folder_name, BUZAI_CSV_NAME)) as f:
-        load_section_data(f,res)
+        load_section_data(f, res)
     return res
 
 
-
-
 def load_section_data(f, res):
+    # 不要
     lines = f.readlines()
     section_index = []
     for i, line in enumerate(lines):
@@ -28,8 +95,6 @@ def load_section_data(f, res):
             data = lines[idx + 1:section_index[i + 1]]
         else:
             data = lines[idx + 1:]
-        # print(section_name)
-        # print(data)
         res[section_name] = data
 
 
--- test_cst_reader.py
+++ test_cst_reader.py
@@ -1,29 +1,60 @@
 import pytest
+import os
 from pytest import approx
+# from cst_reader import read_cst
+from cst_reader import CasstData
 
-from cst_reader import read_cst
 
-
-def print_data(da,section):
+def print_data(da, section):
     for l in da[section]:
         print(l.strip())
 
 
-def test_csv_1():
-    #     s = """SECTION,KAIDAKA,1
-    # 1,100.000000,5700.000000,0.000000,5700.000000,6500.000000,1200.000000
-    # """
-    #     lines = s.split(sep='\n')
-    #     print(lines)
-    #     sec_name = lines[0].split(sep=',')[1]
-    #     print(sec_name)
+def test_load_data():
+    cst = CasstData()
+    cst.load('sample_cst')
+
+    assert float(cst.get_section_data('KAIDAKA', idx=2)) == approx(100.)  # GLからの1FL高さ
+    assert float(cst.get_section_data('KAIDAKA', idx=3)) == approx(5700.)  # GLからの2FL高さ
+    assert float(cst.get_section_data('KAIDAKA', idx=6)) == approx(6500.)  # GLからの最高高さ
+    assert float(cst.get_section_data('KEISAN', idx=21)) == approx(300.)  # BPLから1FL
+    assert float(cst.get_section_data('KEISAN', idx=22)) == approx(310.)  # 1FLから基礎梁天
+    assert float(cst.get_section_data('KEISAN', idx=23)) == approx(0.)  # 2FLから梁天
+    assert float(cst.get_section_data('KEISAN', idx=24)) == approx(0.)  # 3FLから梁天
+
+    assert cst.get_section_data('TORISHIN', idx=3) == ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10',
+                                                       '11', '12', '13', '14', '15', '16', '17', '18', '19',
+                                                       '20', '21', '22', '23', '24', '25', '26',
+                                                       'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L',
+                                                       'M', 'N', 'O', "O'"]  # 通り心名
+
+    assert cst.get_section_data('HASHIRA', idx=4)[:5] == ['C2AK', 'C3K', 'C2A', 'C2BK', 'C2K']
+    assert cst.get_section_data('OOBARI', idx=4) == ['B44', 'B44', 'B44', 'B29', 'B15']
+    assert cst.get_section_data('KOBARI', idx=4)[:5] == ['B34', 'B25', 'B25', 'B29', 'B34']
+    assert cst.get_section_data('NOKIKETA_SAKUZU', idx=4)[:5] == ['P1', 'P1', 'P1', 'P1K', 'P1K']
+
+    assert cst.get_buzai_data('HASHIRA', idx=1) == ['C1', 'C1K', 'C1A', 'C2', 'C2K', 'C2AK', 'C2BK', 'C3', 'C3K', 'C2A']
+    assert cst.get_buzai_data('HASHIRA', idx=6)[0] == "□P-125x125x4.5"
+
+    assert cst.get_buzai_data('OOBARI', idx=1) == ['B44', 'B29', 'B15']
+    assert cst.get_buzai_data('OOBARI', idx=6) == ['H-446x199x8x12', 'H-298x149x5.5x8', 'H-150x75x5x7']
+    assert cst.get_buzai_data('KOBARI', idx=1) == ['B24', 'B25', 'B29', 'B34', 'B39', 'B15']
+    assert cst.get_buzai_data('ETC-NOKIGETA', idx=6) == ["C-125x50x20x2.3", "C-125x50x20x3.2"]
+
+
+    # print(cst.buzai)
+    # for d in cst.get_buzai_data('HASHIRA'):
+    #     print(d.strip())
+    # print(cst.get_buzai_data('HASHIRA', idx=1))
+    # print(cst.get_buzai_data('HASHIRA', idx=6))
+
+
+def test_file_names():
+    # cst = CasstData()
+    # files = cst._buzaikigou_file_names("sample_cst")
+    # # for x in files:
+    # #     print(os.path.basename(x))
+    # for x in files:
+    #     print(os.path.basename(x)[:-4].split(sep='_')[-1]) # 拡張子.csvを除き、_で区切られる最後の単語
 
     pass
-
-
-def test_reading():
-    cst = read_cst("sample_cst")
-    # print(cst['KAIDAKA'])
-    # print_data(cst,'KAIDAKA')
-    # print_data(cst,'TORISHIN')
-    print_data(cst,'GAIHEKI-HONTAI')
