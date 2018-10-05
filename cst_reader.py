4e77775d415314dc61067d4f77053df9704bcc90
mskz1 <mskz.tsuji@me.com>
2018-10-05 15:45:14 +0900

    編集

--- cst_reader.py
+++ cst_reader.py
@@ -1,4 +1,44 @@
+import os
+import sys
+
+BUKKEN_CSV_NAME = "Structure\Analysis\Cooperation_Bukken.csv"
+BUZAI_CSV_NAME = 'Structure\Analysis\Cooperation_Buzai.csv'
 
 
-def read_cst(file):
+def read_cst(folder_name):
+    res = {}
+    with open(os.path.join(folder_name, BUKKEN_CSV_NAME)) as f:
+        load_section_data(f, res)
+    with open(os.path.join(folder_name, BUZAI_CSV_NAME)) as f:
+        load_section_data(f,res)
+    return res
+
+
+
+
+def load_section_data(f, res):
+    lines = f.readlines()
+    section_index = []
+    for i, line in enumerate(lines):
+        if line[:7] == "SECTION":
+            section_index.append(i)
+    for i, idx in enumerate(section_index):
+        section_name = lines[idx].split(sep=',')[1]
+        if i < len(section_index) - 1:
+            data = lines[idx + 1:section_index[i + 1]]
+        else:
+            data = lines[idx + 1:]
+        # print(section_name)
+        # print(data)
+        res[section_name] = data
+
+
+if __name__ == '__main__':
+    # folder_name = "sample_cst"
+    # with open(os.path.join(folder_name, BUKKEN_CSV_NAME)) as bukken:
+    #     print(bukken.name)
+    #
+    # buzai = open(os.path.join(folder_name, BUZAI_CSV_NAME))
+    # print(buzai.name)
+    # buzai.close()
     pass
--- test_cst_reader.py
+++ test_cst_reader.py
@@ -1,9 +1,29 @@
 import pytest
 from pytest import approx
 
-
-
 from cst_reader import read_cst
 
 
+def print_data(da,section):
+    for l in da[section]:
+        print(l.strip())
 
+
+def test_csv_1():
+    #     s = """SECTION,KAIDAKA,1
+    # 1,100.000000,5700.000000,0.000000,5700.000000,6500.000000,1200.000000
+    # """
+    #     lines = s.split(sep='\n')
+    #     print(lines)
+    #     sec_name = lines[0].split(sep=',')[1]
+    #     print(sec_name)
+
+    pass
+
+
+def test_reading():
+    cst = read_cst("sample_cst")
+    # print(cst['KAIDAKA'])
+    # print_data(cst,'KAIDAKA')
+    # print_data(cst,'TORISHIN')
+    print_data(cst,'GAIHEKI-HONTAI')
